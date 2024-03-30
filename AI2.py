a52de30a0fe1c35e0e28123ffb913697e23f6499
Sv1fT1 <vlad.y.pavlenko@gmail.com>
2024-03-30 20:58:48 +0200

    main script + gitinore added

--- AI2.py
+++ AI2.py
@@ -1,23 +1,23 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow import keras
-from tensorflow.keras.layers import Dense
+import keras
+from keras import layers
+from keras import ops
 
 c = np.array([-40, -10, 0, 8, 15, 22, 38])
 f = np.array([-40, 14, 32, 46, 59, 72, 100])
 
 model = keras.Sequential()
-model.add(Dense(units=1, input_shape=(1,), activation='linear'))
-model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
-
-history = model.fit(c, f, epochs=500, verbose=0)
-print("Обучение завершено")
-
-print(model.predict([100]))
-print(model.get_weights())
+model.add(layers.Dense(1, activation="relu", name="layer1"))
+model.compile(
+    optimizer="Adam",
+    loss=keras.losses.BinaryCrossentropy()
+)
+history = model.fit(c, f)
 
 plt.plot(history.history['loss'])
 plt.grid(True)
