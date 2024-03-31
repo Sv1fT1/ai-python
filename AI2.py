f2929e4315fe0e5c493be805613066fe1ee85d5a
Sv1fT1 <vlad.y.pavlenko@gmail.com>
2024-03-31 20:24:24 +0300

    main script + gitinore added

--- AI2.py
+++ AI2.py
@@ -1,18 +1,19 @@
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
 from keras import layers
 from keras import ops
+import tensorflow
 
 c = np.array([-40, -10, 0, 8, 15, 22, 38])
 f = np.array([-40, 14, 32, 46, 59, 72, 100])
 
 model = keras.Sequential()
-model.add(layers.Dense(1, activation="relu", name="layer1"))
+model.add(layers.Dense(1, activation="relu"))
 model.compile(
     optimizer="Adam",
     loss=keras.losses.BinaryCrossentropy()
